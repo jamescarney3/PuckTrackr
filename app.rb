e0710b45bf66cfbfa45258dfbfae5dbd19df1c0e
James Carney <jmcarneyiii@gmail.com>
2015-11-22 14:50:03 -0500

    Fix script, use Rack::Static to serve js files in /lib

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require "cuba"
+require "mote"
+require "mote/render"
+
+Cuba.plugin(Mote::Render)
 
 Cuba.define do
   on root do
-    res.write("Hello world!")
+    res.write partial("home")
   end
 
-  on "about" do
-    res.write("This is a sandbox application for trying things out with the
-              Cuba gem. In development it runs on a puma server.")
+  on get do
+    on "about" do
+      res.write("This is a sandbox application for trying things out with the
+                Cuba gem. In development it runs on a puma server.")
     end
+  end
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require "./app"
 
+use Rack::Static, :urls => ["/lib"]
+
 run(Cuba)
