5023e384059337c7c9d20ee880cdfbb2631817fd
James Carney <jmcarneyiii@gmail.com>
2015-11-28 20:44:01 -0500

    Add get_user_by_cookie method

--- db_utils.rb
+++ db_utils.rb
@@ -11,9 +11,20 @@ con.close if con
 def insert_user(user_cookie, user_numbers)
   con = open_connection
   numbers = parse_array_for_db(user_numbers)
-
   con.exec "INSERT INTO users VALUES(\'#{user_cookie}\', \'#{numbers}\')"
+  con.close if con
+end
 
+def get_user_by_cookie(cookie)
+  con = open_connection
+  user_attrs = con.exec("SELECT * FROM users WHERE cookie = \'#{cookie}\'").values
+  con.close if con
+  user_attrs.empty? ? nil : user_attrs
+end
+
+def delete_user(user_cookie)
+  con = open_connection
+  con.exec "DELETE FROM users WHERE cookie = \'#{user_cookie}\'"
   con.close if con
 end
 
