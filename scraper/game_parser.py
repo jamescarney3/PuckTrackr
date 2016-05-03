e3fb9627eb427286a6597f342a62303adde91638
James Carney <jmcarneyiii@gmail.com>
2016-05-03 12:56:32 -0400

    Refine parsers, let scraper save a test json blob

--- scraper/game_parser.py
+++ scraper/game_parser.py
@@ -7,9 +7,7 @@ def parse_game(visitor_info, home_info, game_info):
     visitor_info_vals = []
     home_info_vals = []
     game_info_vals = []
-    event_options = {}
-
-
+    game_options = {}
 
     for td in visitor_info('td'):
         visitor_info_vals.append(whiteSpaceReducer.sub(' ', td.get_text(' ', strip=True)))
@@ -20,8 +18,7 @@ def parse_game(visitor_info, home_info, game_info):
     for td in game_info('td'):
         game_info_vals.append(whiteSpaceReducer.sub(' ', td.get_text(' ', strip=True)))
 
-
-    game_options['serial'] = re.compile('\d').sub('', game_info_vals[6])
+    game_options['serial'] = re.compile('\D').sub('', game_info_vals[6])
     # in progress - make this work with url_id for a given game
     # if game_info_vals[2]:
     #     game_options['season'] = 'playoffs'
@@ -30,8 +27,5 @@ def parse_game(visitor_info, home_info, game_info):
     # game_options['arena'] = game_info_vals[4] ---- regexed for arena name
     # game_options['start'] = game_info_vals[5] ---- regexed for start time
     # game_options['end'] = game_info_vals[5] ---- regexed for end time
-
-    
-
 
     return game_options
